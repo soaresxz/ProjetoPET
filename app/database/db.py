3447dc359cb584f8db630623ec134fbbc51118f0
Gabrielhjk <gabrieldejesusfc.12@gmail.com>
2026-05-04 19:16:39 -0300

    ajustando conexão com o banco

--- app/database/db.py
+++ app/database/db.py
@@ -1,20 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./data/pet_qr.db"
+DATABASE_URL = "postgresql://admin:admin@postgres:5432/pet_db"
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False},
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 Base = declarative_base()
     
-
-
 def get_db():
     db = SessionLocal()
     try:
